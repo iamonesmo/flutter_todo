800cd6e7bebab553c95a5ccfe73cb0d92f41663f
Onesmo Kasongo <kasongoonesmo33@gmail.com>
2021-04-09 16:26:07 +0530

    refactored code

--- flutter_todo/lib/main.dart
+++ flutter_todo/lib/main.dart
@@ -1,8 +1,12 @@
-// Imoort MaterialApp and other widgets which we can use to quickly create a material app
+// Import MaterialApp and other widgets which we can use to quickly create a material app
 import 'package:flutter/material.dart';
 
+// Code written in Dart starts exectuting from the main function. runApp is part of
+// Flutter, and requires the component which will be our app's container. In Flutter,
+// every component is known as a "widget".
 void main() => runApp(new TodoApp());
 
+// Every component in Flutter is a widget, even the whole app itself
 class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -18,21 +22,19 @@ class TodoList extends StatefulWidget {
 class TodoListState extends State<TodoList> {
   List<String> _todoItems = [];
 
-  //this will be called each time the app button is pressed
   void _addTodoItem(String task) {
+    // Only add the task if the user actually entered something
     if (task.length > 0) {
+      // Putting our code inside "setState" tells the app that our state has changed, and
+      // it will automatically re-render the list
       setState(() => _todoItems.add(task));
     }
   }
 
-  // much like _addTodoItem, this modifies the array of todo strings and
-  // notifies the app the state has changed by using setastate
-
-  void _removeTodoList(int index) {
+  void _removeTodoItem(int index) {
     setState(() => _todoItems.removeAt(index));
   }
 
-  // Show an alert dialog asking the user to confirm that the task is done
   void _promptRemoveTodoItem(int index) {
     showDialog(
         context: context,
@@ -40,23 +42,28 @@ class TodoListState extends State<TodoList> {
           return new AlertDialog(
               title: new Text('Mark "${_todoItems[index]}" as done?'),
               actions: <Widget>[
-                new TextButton(
+                new FlatButton(
                     child: new Text('CANCEL'),
+                    // The alert is actually part of the navigation stack, so to close it, we
+                    // need to pop it.
                     onPressed: () => Navigator.of(context).pop()),
-                new TextButton(
+                new FlatButton(
                     child: new Text('MARK AS DONE'),
                     onPressed: () {
-                      _removeTodoList(index);
+                      _removeTodoItem(index);
                       Navigator.of(context).pop();
                     })
               ]);
         });
   }
 
-  //Build the whole list of todo items
+  // Build the whole list of todo items
   Widget _buildTodoList() {
     return new ListView.builder(
       itemBuilder: (context, index) {
+        // itemBuilder will be automatically be called as many times as it takes for the
+        // list to fill up its available space, which is most likely more than the
+        // number of todo items we have. So, we need to check the index is OK.
         if (index < _todoItems.length) {
           return _buildTodoItem(_todoItems[index], index);
         }
@@ -64,7 +71,7 @@ class TodoListState extends State<TodoList> {
     );
   }
 
-  //build a single todo item
+  // Build a single todo item
   Widget _buildTodoItem(String todoText, int index) {
     return new ListTile(
         title: new Text(todoText), onTap: () => _promptRemoveTodoItem(index));
@@ -76,7 +83,7 @@ class TodoListState extends State<TodoList> {
       appBar: new AppBar(title: new Text('Todo List')),
       body: _buildTodoList(),
       floatingActionButton: new FloatingActionButton(
-          onPressed: _pushAddTodoScreen, // opens a new screen
+          onPressed: _pushAddTodoScreen,
           tooltip: 'Add task',
           child: new Icon(Icons.add)),
     );
@@ -85,8 +92,8 @@ class TodoListState extends State<TodoList> {
   void _pushAddTodoScreen() {
     // Push this page onto the stack
     Navigator.of(context).push(
-        // MaterialPageRoute will automatically animate the screen entry, as well
-        // as adding a back button to close it
+        // MaterialPageRoute will automatically animate the screen entry, as well as adding
+        // a back button to close it
         new MaterialPageRoute(builder: (context) {
       return new Scaffold(
           appBar: new AppBar(title: new Text('Add a new task')),
