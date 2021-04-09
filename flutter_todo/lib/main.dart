4a034727971b78e83cf1e9b7148be7c51b7326dd
Onesmo Kasongo <kasongoonesmo33@gmail.com>
2021-04-09 14:35:43 +0530

    added a floating action button that allows items to get added to the list

--- flutter_todo/lib/main.dart
+++ flutter_todo/lib/main.dart
@@ -1,6 +1,4 @@
 // Imoort MaterialApp and other widgets which we can use to quickly create a material app
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(new TodoApp());
@@ -18,8 +16,46 @@ class TodoList extends StatefulWidget {
 }
 
 class TodoListState extends State<TodoList> {
+  List<String> _todoItems = [];
+
+  //this will be called each time the app button is pressed
+  void _addTodoItem() {
+    //Putting the item inside "setState" tell the app that our state has changed,
+    //and it will automatically re-render the list
+    setState(() {
+      int index = _todoItems.length;
+      _todoItems.add('Item' + index.toString());
+    });
+  }
+
+  //Build the whole list of todo items
+  Widget _buildTodoList() {
+    return new ListView.builder(
+      itemBuilder: (context, index) {
+        //itemBuilder will be automatically called asm many times as it takes for
+        //the list to fill up its available space, which is most likely more than
+        //number of todo items we have. So we need to check the index is OK.
+        if (index < _todoItems.length) {
+          return _buildTodoItem(_todoItems[index]);
+        }
+      },
+    );
+  }
+
+  // Build a single todo item
+  Widget _buildTodoItem(String todoText) {
+    return new ListTile(title: new Text(todoText));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(appBar: new AppBar(title: new Text('Todo List')));
+    return new Scaffold(
+      appBar: new AppBar(title: new Text('Todo List')),
+      body: _buildTodoList(),
+      floatingActionButton: new FloatingActionButton(
+          onPressed: _addTodoItem,
+          tooltip: 'Add task',
+          child: new Icon(Icons.add)),
+    );
   }
 }
